2c82170bdf7c2314bfad0713cdf310a7e29e95c1
Yasar Laurin Plückebaum <yasar.plueckebaum@uni-bielefeld.de>
2023-10-20 22:05:41 +0200

    Sheet 1,Task 1 Code done

--- Sheet1/sheet1_1.py
+++ Sheet1/sheet1_1.py
@@ -1,4 +1,6 @@
 #import scikitlearn
+import matplotlib.pyplot
+import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import scipy as sp
 import numpy as np
@@ -13,4 +15,16 @@ def load_data():
 
 if __name__ == '__main__':
     set1,set2 = load_data()
+    fig,axes = plt.subplots(2,2)
+    axes[0,0].scatter(set1.T[0],set1.T[1])
+    axes[1,0].scatter(set2.T[0],set2.T[1])
+    whiten_data = PCA(whiten=True,n_components=2) # both 2 dim?
+    set1_w = whiten_data.fit_transform(set1)
+    set2_w = whiten_data.fit_transform(set2)
+    axes[0,1].scatter(set1_w.T[0],set1_w.T[1])
+    axes[1,1].scatter(set2_w.T[0],set2_w.T[1])
+
+
+    fig.show()
+
     # write your code here
