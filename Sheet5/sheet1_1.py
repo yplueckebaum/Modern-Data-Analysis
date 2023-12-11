5fb28ce9deace99a0b362e52b514c925af1b0587
Yasar Laurin Plückebaum <yasar.plueckebaum@uni-bielefeld.de>
2023-12-11 13:36:00 +0100

    ups xd

---
+++ Sheet5/sheet1_1.py
@@ -0,0 +1,41 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cvxpy as cp
+
+# generate 10 random R2 vectors
+
+def find_MEB(S):
+    # Find the minimum enclosing ball of a set of points in R^n
+    # S: set of points
+    # Return: center, radius
+
+    # init one alpha for each datapoint
+    alpha = cp.Variable(S.shape[0])
+    prob = cp.Problem(cp.Minimize(alpha * S * alpha.T - cp.sum(alpha[i])*S[i].T*S[i]),
+                      [alpha >= 0,
+                       cp.sum(alpha) == 1])
+    prob.solve()
+
+    center = prob.value
+
+    return center, radius
+
+
+def find_MEB_approx(S, eps):
+    # Find the minimum enclosing ball of a set of points in R^n
+    # S: set of points
+    # eps: precision
+    # Return: center, radius
+
+ 
+    return center, radius
+
+np.random.seed(42)
+
+test = np.random.rand(10,2)
+
+        
+
+
+   
